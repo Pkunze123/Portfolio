37fd18f7b67c1359638f69ac9eac9a871e9a5098
Phil Kunze <pmkwoodworking@gmail.com>
2023-04-30 18:36:07 -0500

    Add gettingToKnowYou.py

---
+++ Getting to know you.py
@@ -0,0 +1,6 @@
+name = input("Hi, What is your name?\n")
+food = input("what is your favorite food?\n")
+music = input("what is your favorite music?\n")
+located = input("Where are you located?\n")
+print(f"Great to meet you, {name}!\n"
+      f"I also love {food}, listening to {music} and I would love to visit {located}.")
