cf9d1002c4589ab506abfa6274ec1aec06c7577c
Phil Kunze <pmkwoodworking@gmail.com>
2023-05-01 19:51:23 -0500

    Add disney_princess_game.py

--- Getting to know you.py
+++ Getting to know you.py
@@ -2,5 +2,6 @@ name = input("Hi, What is your name?\n")
 food = input("what is your favorite food?\n")
 music = input("what is your favorite music?\n")
 located = input("Where are you located?\n")
-print(f"Great to meet you, {name}!\n"
-      f"I also love {food}, listening to {music} and I would love to visit {located}.")
+print(f"Great to meet you, {name}! "
+      f"I also love {food} and listening to {music}. "
+      f"I would love to visit {located}.")
