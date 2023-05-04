0d944165f21a826f031bc4c182ab4fd95402e07c
Phil Kunze <pmkwoodworking@gmail.com>
2023-05-03 22:27:02 -0500

    update disney_princess_game.py

---
+++ Disney_Princess_game1.0.py
@@ -0,0 +1,50 @@
+print("Which Disney Princess are you?")
+while True:
+    elsa = input("Do you have ice powers?\n")
+    if elsa == "yes":
+        print("YOU are Elsa!")
+        break
+    else:
+        ariel = input("Do you use a Dinglehopper?\n")
+        if ariel == "yes":
+            print("YOU are Ariel!")
+            break
+        else:
+            tiana = input("Did you kiss a frog?\n")
+            if tiana == "yes":
+                print("YOU are Tiana!")
+                break
+            else:
+                mereda = input("Is your mom a bear?\n")
+                if mereda == "yes":
+                    print("YOU are Mereda!")
+                    break
+                else:
+                    Mulan = input("Did you pretend to be a boy?\n")
+                    if Mulan == "yes":
+                        print("YOU are Mulan!")
+                        break
+                    else:
+                        Rapunzel = input("Do you have incredibly long hair?\n")
+                        if Rapunzel == "yes":
+                            print("YOU are Rapunzel!")
+                            break
+                        else:
+                            Venelope = input("Do you race karts?\n")
+                            if Venelope == "yes":
+                                print("YOU are Venelope Von Schweets!")
+                                break
+                            else:
+                                Belle = input(
+                                    "Do you suffer from stockholm syndrome?\n")
+                                if Belle == "yes":
+                                    print("YOU are Belle!")
+                                    break
+                                else:
+                                    Cinderella = input(
+                                        "Did you lose a glass slipper?\n")
+                                    if Cinderella == "yes":
+                                    print("YOU are Cinderella!")
+                                    break
+                                    else:
+                                        print("You are NOT a Disney Princess...")
